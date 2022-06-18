19c8ee29f662a4f997f7e499e41270fff5dfa746
Wagner Alves <guinn83@hotmail.com>
2022-06-18 18:57:54 -0300

    ChessException and ChessPosition

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,16 @@
 package application;
 
 import chess.ChessMatch;
+import chess.ChessPosition;
 
 public class Main {
     public static void main(String[] args) {
         ChessMatch chessMatch = new ChessMatch();
         UI.printBoard(chessMatch.getPieces());
+
+        ChessPosition cp = new ChessPosition('c', 2);
+
+
+        System.out.println();
     }
 }
