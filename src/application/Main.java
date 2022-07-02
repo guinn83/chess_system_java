8808ae5e9e15ace592927994a515c18c878ebb97
Wagner Alves <guinn83@hotmail.com>
2022-07-02 15:34:05 -0300

    Special move - Promotion update

--- src/application/Main.java
+++ src/application/Main.java
@@ -40,7 +40,11 @@ public class Main {
 
                 if (chessMatch.getPromoted() != null) {
                     System.out.print("Digite a peça para promoção (B/N/R/Q): ");
-                    String type = sc.nextLine();
+                    String type = sc.nextLine().toUpperCase();
+                    while (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
+                        System.out.print("Digite a peça para promoção (B/N/R/Q): ");
+                        type = sc.nextLine().toUpperCase();
+                    }
                     chessMatch.replacePromotedPiece(type);
                 }
 
